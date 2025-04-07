34ee6a77c1ef1504b7bc1291054dd35e08e88ffa
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-04-07 20:59:04 +0300

    Add player rocket prefab and sandbox scene
    - Created a new prefab for the Player Rocket.
    - Added a new Sandbox scene with various game objects including a Main Camera, Directional Light, and Ground.
    - Implemented Movement script for player control with thrust and rotation mechanics.
    - Updated Editor Settings to enable Play Mode options.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
+    [SerializeField] float thrustStrength = 100f;
+    [SerializeField] float rotationStrength = 100f;
+    Rigidbody rigidBody;
+
+    void OnEnable()
+    {
+        thrust.Enable();
+        rotation.Enable();
+    }
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
+        if (thrust.IsPressed())
+        {
+            rigidBody.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+    }
+}
