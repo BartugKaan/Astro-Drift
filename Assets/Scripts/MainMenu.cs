a77db8e32418c2acee0f238bdfc7948921142eac
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2025-04-18 22:02:36 +0300

    Add new shaders and assets for TextMesh Pro
    - Introduced new shader files: TMPro_Mobile.cginc, TMPro_Properties.cginc, and TMPro_Surface.cginc to enhance rendering capabilities.
    - Added corresponding meta files for the new shaders to ensure proper asset management.
    - Included EmojiOne sprite assets with attribution text and JSON configuration for sprite mapping.
    - Updated EditorBuildSettings to include the new main scene for better project organization.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] GameObject playerRocket;
+    [SerializeField] float movementSpeed = 5;
+
+    bool isPlayPressed = false;
+
+    public void PlayGame()
+    {
+        isPlayPressed = true;
+        Invoke("LoadFirstLevel", 2);
+    }
+
+    void FixedUpdate()
+    {
+        if (isPlayPressed == true)
+        {
+            playerRocket.transform.Translate(new Vector3(1, 1, 0) * Time.deltaTime * movementSpeed);
+        }
+    }
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void ExitGame()
+    {
+        if (isPlayPressed)
+        {
+            Application.Quit();
+        }
+    }
+
+}
